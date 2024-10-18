4e20e7188db65d6a7acb82cb6a86fb6763595568
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-10-19 04:46:35 +0500

    refactor(polybar): don't display info on "Stopped" playerctl status

--- theme/polybar/scripts/playerctl.sh
+++ theme/polybar/scripts/playerctl.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 song_name=$(playerctl metadata -i "firefox" -f "{{artist}} - {{trunc(title, 20)}}")
-if [[ -n $song_name && $song_name != "No players found" ]]; then
-    echo " $song_name"
-else
+
+if [[ $(playerctl status) == "Stopped" || $song_name == "No players found" ]]; then
     echo "󰎊"
+    exit 0
+fi
+
+if [[ -n $song_name ]]; then
+    echo " $song_name"
 fi
