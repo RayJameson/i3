94f1f6057e3dd263f60f676e26cf7efaeb20a545
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-10-19 04:25:57 +0500

    feat(polybar): playerctl improvements
    added note icons for on/off modes
    if no song is playing just display note, without text

--- theme/polybar/scripts/playerctl.sh
+++ theme/polybar/scripts/playerctl.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-song_name=$(playerctl metadata -f "{{artist}} - {{trunc(title, 20)}}")
-if [[ -n $song_name ]]; then
-    echo "$song_name"
+song_name=$(playerctl metadata -i "firefox" -f "{{artist}} - {{trunc(title, 20)}}")
+if [[ -n $song_name && $song_name != "No players found" ]]; then
+    echo " $song_name"
+else
+    echo "󰎊"
 fi
